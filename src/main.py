c99d667201a1f882881331aed2830835c20f46d0
Orealba <orealbas@gmail.com>
2022-08-25 17:01:09 +0000

    tarea lista

--- src/main.py
+++ src/main.py
@@ -31,19 +31,24 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    allUser = User.query.all() #retorna un arreglo de clases 
+    allUser = list(map(lambda elemento: elemento.serialize(), allUser))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(allUser), 200
 
-    return jsonify(response_body), 200
+@app.route('/user/favorite', methods=['GET'])
+def get_user_favorite():
+    allUserFavorite = Fav_people.query.all() #retorna un arreglo de clases 
+    allUserFavorite = list(map(lambda elemento: elemento.serialize(), allUserFavorite))
+
+    return jsonify(allUserFavorite), 200
 
 @app.route('/people', methods=['GET'])
 def get_people():
     allpeople = People.query.all() #retorna un arreglo de clases 
     allpeople = list(map(lambda elemento: elemento.serialize(), allpeople)) #itero en cada una de las clases y almaceno el resultado de la funcion serialize
-    print(allpeople)
+    
     return jsonify({"resultado": allpeople})
 
 
@@ -74,13 +79,14 @@ def get_one_planet(id):
         
 @app.route("/favorite/people/<int:people_id>", methods=['POST'])
 def add_fav_people(people_id):
+    print("mensaje")
     onepeople = People.query.get(people_id)
     if onepeople:
         new = Fav_people()
         new.user_id = 1
         new.people_id = people_id
-        db.session.add(new) #agrego el registro a la base de datos
-        db.session.commit() #guardar los cambios realizados
+        db.session.add(new) 
+        db.session.commit() 
 
         return jsonify({"mensaje": "Todo salio bien"})
     else:
@@ -100,7 +106,13 @@ def add_fav_planet(planet_id):
     else:
         return jsonify({"resultado": "personaje no existe"})
 
+@app.route('/favorite/people/<int:id>', methods=['DELETE'])
+def delete_one_Fav_people(id):
 
+    Fav_people_delete = Fav_people.query.get(id)
+    db.session.delete(Fav_people_delete)
+    db.session.commit()
+    return jsonify({"mensaje": "Todo salio bien"}),200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
